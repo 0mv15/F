94de00bf2a91efdb7611066813a06289fd3c68d2
mvv015 <97501521+mvv015@users.noreply.github.com>
2022-06-16 17:33:55 +0530

    Update drive.py

--- drive.py
+++ drive.py
@@ -27,7 +27,7 @@ def get_gdrive_service():
 
 service = get_gdrive_service()
 
-folder_id = '1WaW-iuC3A_irzvEg59yPWKpZJkjvKo5K'
+folder_id = '1yDv--4DpuEGWgY1kPP5g-CD6wcObkqQb'
 file_name = f"{file}"
 file_path = f'/usr/src/app/{file_name}'
 
